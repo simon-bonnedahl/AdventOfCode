77bbe33f3300f3acfb81487c3ef269246656ae3f
Simon Bonnedahl <simbo803@student.liu.se>
2022-12-04 06:29:35 +0100

    Day 4

---
+++ 2022/day4/part2.py
@@ -0,0 +1,22 @@
+def main(input):
+    sum = 0
+    for pair in input:
+        e1, e2 = pair.split(",")
+        e1_start, e1_end = map(int, e1.split("-"))
+        e2_start, e2_end = map(int, e2.split("-"))
+        e1_set = set(range(e1_start, e1_end + 1))
+        e2_set = set(range(e2_start, e2_end + 1))
+        if len(e1_set & e2_set) > 0:
+            sum += 1
+
+    print(sum)
+
+        
+
+def readInput():
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+    return [c[:-1] for c in lines]
+
+if __name__ == "__main__":
+    main(readInput())
