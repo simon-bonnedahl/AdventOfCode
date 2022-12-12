e11ff3e8c1f0933f51364a17ff978bfee8f7959f
Simon Bonnedahl <simbo803@student.liu.se>
2022-12-12 11:15:48 +0100

    Day 12

---
+++ 2022/day12/part2.py
@@ -0,0 +1,65 @@
+from collections import deque
+
+def main(input):
+
+    grid = [list(c) for c in input.strip().splitlines()]
+
+    def getNeighbors(pos):
+        neighbors = []
+        neighbors.append((pos[0], pos[1] + 1)) # down
+        neighbors.append((pos[0], pos[1] - 1 )) # up
+        neighbors.append((pos[0] + 1, pos[1])) # right
+        neighbors.append((pos[0] - 1, pos[1])) # left
+        return neighbors
+
+    for y, row in enumerate(grid):
+        for x, char in enumerate(row):
+            if char == "S":
+                grid[y][x] = "a"
+            if char == "E":
+                dst = (x, y)
+                grid[y][x] = "z"
+
+    q = deque()
+    q.append([0, dst])
+
+    visited = set()
+    visited.add(dst)
+
+ 
+
+    #BFS
+    while q:
+        distance, pos = q.popleft()
+        for neighbor in getNeighbors(pos):
+            #Check grid bounds      
+            if neighbor[0] < 0 or neighbor[1] < 0: #X
+                continue   
+            if neighbor[0] >= len(grid[0]) or neighbor[1] >= len(grid): #Y
+                continue   
+            #Check visited
+            if neighbor in visited:
+                continue
+            #Check elevation diff
+            if ord(grid[neighbor[1]][neighbor[0]]) - ord(grid[pos[1]][pos[0]]) < -1:
+                continue
+            #Check if we found the destionation
+            if grid[neighbor[1]][neighbor[0]] == "a":
+                print(distance + 1)
+                exit()
+                
+            #Add neighbor to the queue
+            visited.add(neighbor)
+            q.append([distance + 1, neighbor])
+
+
+           
+
+def readInput():
+    with open("input.txt", "r") as f:
+        lines = f.read()
+    return lines
+
+if __name__ == "__main__":
+    main(readInput())
+
